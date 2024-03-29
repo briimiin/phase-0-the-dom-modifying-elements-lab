37ab97d14b4fc9d3f3a1e31063a78b1844f0b27b
briimiin <kipkorirbrimin005@gmail.com>
2024-03-29 13:31:07 +0300

    modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement = document.querySelector('main');
+mainElement.remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.setAttribute('id','victory');
+newHeader.textContent = 'Brimin is the champion';
